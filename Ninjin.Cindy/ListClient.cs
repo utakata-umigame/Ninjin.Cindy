b451ae614ca14596f25299e4da89af18cae79974
Kohei <atihsa.mihs.ne.ho.da@gmail.com>
2017-11-14 12:41:11 +0900

    Integrate ListClient

--- Ninjin.Cindy/ListClient.cs
+++ Ninjin.Cindy/ListClient.cs
@@ -1,43 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using System.Net;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Ninjin.Cindy.Model;
 
 namespace Ninjin.Cindy
 {
     public class ListClient:WebClient
     {
-        protected virtual string endPoint
+        public ListClient(ModelType type)
         {
-            get { return ""; }
+            ModelType = type;
+            EndPoint = SelectEndPoint(type);
+            Objects = new List<CindyModel>();
+            DownloadStringCompleted += ListClient_DownloadStringCompleted;
         }
 
+        
+        const string mondaiListEndPoint = "http://heyrict.pythonanywhere.com/api/mondai_list";
+        const string commentListEndPoint = "http://heyrict.pythonanywhere.com/api/comment";
+        const string userListEndPoint = "http://heyrict.pythonanywhere.com/api/";
+        const string starListEndPoint = "http://heyrict.pythonanywhere.com/api/star";
+        public ModelType ModelType { get; private set; }
+        public string EndPoint { get; }
         public Action Load = delegate { };
-        public List<object> Objects{ get; protected set; }
+        public List<CindyModel> Objects{ get;}
         /// <summary>
         /// Fetch Object list
         /// </summary>
-        protected void Fetch()
+        public void Fetch()
         {
-            if (endPoint == null)
+            if (EndPoint == null)
             {
                 throw new ArgumentNullException("no endpoint specified.");
             }
-            var result = Regex.Unescape(DownloadString(new Uri(endPoint)));
+            var result = Regex.Unescape(DownloadString(new Uri(EndPoint)));
             Parse(result);
         }
-        protected void StartFetch()
+        public void FetchDataAsync()
         {
-            DownloadStringAsync(new Uri(endPoint));
+            DownloadStringAsync(new Uri(EndPoint));
+        }
+        /// <summary>
+        /// callback
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ListClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            Parse(e.Result);
+            Load();
         }
         /// <summary>
         /// create object list based on raw json response.
         /// to be implemented in inherited class.
-        /// </summary>
+        /// /summary>
         /// <param name="rawRes"></param>
-        protected virtual void Parse(string rawRes)
+        private void Parse(string rawRes)
         {
+            dynamic obj = JsonConvert.DeserializeObject(rawRes);
+            var res = (IEnumerable<object>)obj.data;//This step depends on Json format.
+            Objects.AddRange(res.Select(x => CindyModel.FromJSON(x, ModelType)));
+        }
+        private string SelectEndPoint(ModelType type)
+        {
+            switch (type)
+            {
+                case ModelType.Mondai:
+                    return mondaiListEndPoint;
+                case ModelType.User:
+                    return userListEndPoint;
+                case ModelType.Comment:
+                    return commentListEndPoint;
+                case ModelType.Star:
+                    return starListEndPoint;
+                default:
+                    throw new ArgumentException();
+            }
         }
     }
 }
